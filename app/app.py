28986fb21650dc397443b00b82152735892f0925
Michael Müller <Micha_Mueller@gmx.de>
2019-12-27 18:19:21 +0100

    local route

--- app/app.py
+++ app/app.py
@@ -5,16 +5,21 @@ import sys
 
 mysql = MySQL()
 app = Flask(__name__)
-app.config['MYSQL_HOST'] = sys.argv[1]
+
+try:
+    app.config['MYSQL_HOST'] = sys.argv[1]
+except:
+    app.config['MYSQL_HOST'] = '127.0.0.1'
+    app.config['MYSQL_PORT'] = 13306
+
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = 'password'
 app.config['MYSQL_DB'] = 'tel_dir'
 mysql.init_app(app)
 
-
 @app.route("/")
 def hello():
- return 'Hello viewer ,welcome . Please  check /tel_dir for telephone directory details'
+ return 'Hallo Benutzer Willkommen!. Please  check /tel_dir for telephone directory details'
 
 @app.route("/tel_dir")
 def index():
@@ -24,10 +29,8 @@ def index():
  mysql.connection.commit()
  return render_template('test_table.html', table=rows)
 
-
-
 if __name__=="__main__":
  app.run(
-  host="0.0.0.0",
-  port=5000
+#  host="0.0.0.0",
+#  port=5000
 )
